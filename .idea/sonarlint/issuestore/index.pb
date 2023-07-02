718552d6854784339397a3a0fae2f21c91fcadc6
Kai Kiwitz <kai.kiwitz@gmail.com>
2023-07-02 08:28:26 +0200

    introducing classes and class methods

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+^
+..idea/inspectionProfiles/profiles_settings.xml,1/e/1e9075f5bf079c01ef2c910709e91a497d262080
+U
+%instances/atlas/brainAtlas/DKA.jsonld,8/4/84796173248a5aaedcd170551117dec1432fe436
+V
+&instances/atlas/brainAtlas/AAL1.jsonld,c/d/cd28dfb124cda16ade04e2a8ade7bf78bc8b9a58
---
+++ helper_code/Atlas.py
@@ -0,0 +1,99 @@
+import os.path
+import glob
+import openMINDS.version_manager
+import json
+import Mars_data_structures
+
+
+class Atlas:
+
+    # class variables used for json Instances
+    has_entity_listofdic = []
+    has_terminology_listofdic = []
+    author_listofdic = []
+    has_version_listofdic = []
+    version_https = "https://openminds.ebrains.eu/instances/brainAtlasVersion/"
+    entity_https = "https://openminds.ebrains.eu/instances/parcellationEntity/"
+    person_https = "https://openminds.ebrains.eu/instances/person/"
+
+    # intialize openMinds instance creator
+    openMINDS.version_manager.init()
+    openMINDS.version_manager.version_selection('v3')
+    helper = openMINDS.Helper()
+    basic = helper.create_collection()
+
+    def __init__(self, atlas_dir, authors, versions, description, shortname, fullname, homepage,
+                 documentation, abbreviation, *areas):
+        self.path = atlas_dir
+        self.authors = authors
+        self.versions = versions
+        self.decription = description
+        self.shortname = shortname
+        self.fullname = fullname
+        self.homepage = homepage
+        self.documentation = documentation
+        self.abbreviation = abbreviation
+        self.areas = areas
+
+    @classmethod
+    def author_gen(cls, instance):
+        for item in instance.authors:
+            for name in item.keys():
+                if name is None:
+                    continue
+                else:
+                    author_dic = {"@id": f"{cls.person_https}{name}"}
+                    cls.author_listofdic.append(author_dic)
+
+    @classmethod
+    def entity_gen(cls, abb, areas):
+        for set in areas:
+            for area in set:
+                if area is not None:
+                    entity_dic = {"@id": f"{cls.entity_https}{abb}_{area}"}
+                    cls.has_entity_listofdic.append(entity_dic)
+        return cls.has_entity_listofdic
+
+    @classmethod
+    def terminology_gen(cls, instance):
+        has_terminology_dic = {"@type": "https://openminds.ebrains.eu/sands/ParcellationTerminology", "definedIn": None,
+                               "hasEntity": cls.entity_gen(instance.abbreviation, instance.areas)}
+        cls.has_terminology_listofdic = has_terminology_dic
+
+    @classmethod
+    def version_gen(cls, instance):
+        for dic in instance.versions:
+            for version in dic.keys():
+                has_version_dic = {"@id": f"{cls.version_https}{version}"}
+                cls.has_version_listofdic.append(has_version_dic)
+
+    @classmethod
+    def generate_instances(cls, instance):
+        # generate atlas with necessary information
+        atlas = cls.basic.add_SANDS_brainAtlas(description=instance.description, shortName=instance.shortname,
+                                               fullName=instance.fullname,
+                                               author=cls.author_listofdic,
+                                               hasTerminology=cls.has_terminology_listofdic,
+                                               hasVersion=cls.has_version_listofdic)
+        # adding additional info
+        cls.basic.get(atlas).digitalIdentifier = [{"@id": f"{instance.documentation}"}]
+        cls.basic.get(atlas).homepage = instance.hompage
+        # custodian
+        cls.basic.get(atlas).custodian = [{"@id": "https://openminds.ebrains.eu/instances/person/kleinArno"}]
+        # saving in class-specific path
+        cls.basic.save("./instances/PythonLibrary/")
+
+    @staticmethod
+    def generate_openminds_instances(instance):
+        # copy contents
+        latest = max(glob.glob("./instances/PythonLibrary/brainAtlas/*jsonld"))
+        with open(latest, 'r') as f:
+            data = json.load(f)
+            atlas_name = os.path.basename(instance.path).replace(".jsonld", "")
+            data["@id"] = f"https://openminds.ebrains.eu/instances/brainAtlas/{atlas_name}"
+            f.close()
+        # write content to new file
+        json_target = open(instance.path, "w")
+        json.dump(data, json_target, indent=2, sort_keys=True)
+        json_target.write("\n")
+        json_target.close()
