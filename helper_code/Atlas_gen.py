bbc65d5a9364a9cba3b5fa1098917f627e57b76f
Kai Kiwitz <kai.kiwitz@gmail.com>
2023-08-17 14:39:02 +0200

    introducing classes and class methods

--- helper_code/Atlas_gen.py
+++ helper_code/Atlas_gen.py
@@ -3,33 +3,32 @@ import Mars_data_structures
 from Atlas import AtlasGen
 from AtlasVersion import AtlasVersionGen
 from ParcellationEntity import ParcellationEntityGen
-# get the Mars Atlas data
+
+# get Mars Atlas data
 authors, documentation, description, abbreviation, fullname, shortname, \
     homepage, versions, areas_versions_hierachry, areas_unique, parents_unique = Mars_data_structures.data_structures()
 
-
-# create the atlas dir
-# atlas dir
+# atlas dirs
 j = ".jsonld"
 atlas_dir = f"/home/kiwitz1/PycharmProjects/openMINDS_SANDS/instances/atlas/brainAtlas/{fullname}{j}"
 version_dir = "/home/kiwitz1/PycharmProjects/openMINDS_SANDS/instances/atlas/brainAtlasVersion/"
 entity_dir = f"/home/kiwitz1/PycharmProjects/openMINDS_SANDS/instances/atlas/parcellationEntity/{abbreviation}/"
 os.mkdir(entity_dir)
 
-# create the Atlas Instance and call all Atlas methods
+# create the Atlas
 Mars = AtlasGen(atlas_dir, authors, versions, description, shortname, fullname, homepage, documentation, abbreviation,
              areas_unique, parents_unique)
 AtlasGen.generate_instances(Mars)
 AtlasGen.generate_openminds_instances(Mars)
 
-# create Atlas Versions and call altlas versions methods
+# create Atlas Versions
 for dic in versions:
     for version in dic.keys():
         MarsVersion = AtlasVersionGen(version_dir, version, dic, areas_versions_hierachry)
         AtlasVersionGen.generate_instances(MarsVersion)
         AtlasVersionGen.generate_openminds_instances(MarsVersion)
 
-# create Atlas Parcellation Instances
-Parcellation_Entitites = ParcellationEntityGen(entity_dir, abbreviation, areas_versions_hierachry,areas_unique, parents_unique )
+# create Atlas Parcellations
+Parcellation_Entitites = ParcellationEntityGen(entity_dir, abbreviation, areas_versions_hierachry,areas_unique, parents_unique)
 ParcellationEntityGen.generate_instances(Parcellation_Entitites)
 
